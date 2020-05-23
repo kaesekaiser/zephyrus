eaf13abf7277557874cce0119ba9b94acb9a5d12
Chris <cbain919@gmail.com>
2020-05-23 02:32:31 -0400

    hhhhh

--- bot/main.py
+++ bot/main.py
@@ -351,7 +351,7 @@ async def on_message(message: discord.Message):
         await message.channel.send(zeph.emojis.get("o7", "o7"))
     if zeph.user in message.mentions and re.search(r"^(.*\s)?o7(\s.*)?$", message.content):
         await message.channel.send("o7")
-    if message.content == "h" and message.author != zeph.user:
+    if re.fullmatch("h+", message.content) and message.author != zeph.user:
         await message.channel.send(zeph.emojis.get("aitch", "h"))
 
     if zeph.channelLink is not None and zeph.channelLink.should_activate(message):
