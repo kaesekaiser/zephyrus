28a3eaba24c76c2bbce49b499ca5a356060cd40e
Chris <cbain919@gmail.com>
2021-01-18 20:27:13 -0500

    Add one more testing server to the list

--- bot/startup.py
+++ bot/startup.py
@@ -510,6 +510,6 @@ def should_await(func: callable):
 blue = hexcol("3B88C3")  # color that many commands use
 testing_emote_servers = [  # servers that either are my testing server or that I use only for emote storage
     405184040161771522, 516004299785109516, 516336805151506449, 516017413729419265, 516044646942638090,
-    516015721998843904, 516079973447237648, 528460450069872640
+    516015721998843904, 516079973447237648, 528460450069872640, 800832873854271528
 ]
 activity_channel = 791776037893308417  # channel ID for the channel in which zeph sends ready + error messages to me
