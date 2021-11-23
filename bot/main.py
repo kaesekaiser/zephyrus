388f0044cf90a1b002a4dd4195f2625add02e484
Chris <cbain919@gmail.com>
2021-11-23 11:34:28 -0500

    Narrow scope of X-SAMPA regex

--- bot/main.py
+++ bot/main.py
@@ -419,7 +419,7 @@ async def on_message(message: discord.Message):
 
     if (not message.guild or zeph.server_settings[message.guild.id].sampa) and message.author != zeph.user:
         sampas = []
-        for match in list(re.finditer(r"(?<=x)(\[.*?]|/.*?/)", message.content)):
+        for match in list(re.finditer(r"(?<=[^a-zA-Z0-9/\\]x)(\[.*?]|/.*?/)|(?<=^x)(\[.*?]|/.*?/)", message.content)):
             sampas.append(convert_sampa(match[0]))
         if sampas:
             await message.channel.send("\n".join(sampas))
