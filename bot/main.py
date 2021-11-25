9e0408ba06bd1339b234dbbe1b64d0b85c894a8a
Chris <cbain919@gmail.com>
2021-11-25 11:08:00 -0500

    Change SAMPA regex again

--- bot/main.py
+++ bot/main.py
@@ -419,9 +419,9 @@ async def on_message(message: discord.Message):
 
     if (not message.guild or zeph.server_settings[message.guild.id].sampa) and message.author != zeph.user:
         sampas = []
-        for match in list(re.finditer(r"((?<=\Wx)|(?<=^x))(\[.*?]|/.*?/)", message.content)):
+        for match in list(re.finditer(r"((?<=[^a-zA-Z0-9/\\_]x)|(?<=^x))(\[[^\s].+?]|/[^\s].+?/)", message.content)):
             sampas.append(convert_x_sampa(match[0]))
-        for match in list(re.finditer(r"((?<=\Wz)|(?<=^z))(\[.*?]|/.*?/)", message.content)):
+        for match in list(re.finditer(r"((?<=[^a-zA-Z0-9/\\_]z)|(?<=^z))(\[[^\s].+?]|/[^\s].+?/)", message.content)):
             sampas.append(convert_z_sampa(match[0]))
         if sampas:
             await message.channel.send("\n".join(sampas))
